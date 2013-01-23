0262f8c00878ac5fcb2a82db7733df20a5b27fc7
Ruben Epong <rubenjohne@gmail.com>
2013-01-22 16:22:18 -0800

    initial site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thirty::Application.config.secret_token = '025e280d9fcc28ebcce69e1659bed08a33e3bfea073297fae1ca7b82159fa69b127f893e9ea8b29bada7dc6030ce2db7c44e4665158c240716c21ae6da215dd3'
